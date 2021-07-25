b9ffc4647ed161acbd2ce998991e84ac4ff54073
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2021-07-25 10:43:39 +0100

    Exercise A-list of names  completed
    listed of names

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,11 +1,23 @@
-function listOfNames(arrayOfPeople) {
-  let content = document.querySelector("#content");
-}
 
-let people = [
+
+const people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
   { name: "Boris", job: "Prime Minister" },
 ];
+
+function listOfNames(arrayOfPeople) {
+  const content = document.querySelector("#content");
+   const  h1 = document.createElement("h1");
+    const h2 = document.createElement("h2");
+ for (const i =0; i < people.length; i++)  {
+    h1.innerHTML = `${people[i].name}`;
+    h2.innerHTML = `${people[i].job}`
+
+    document.getElementById("content").appendChild(h1);
+    document.getElementById("content").appendChild(h2);
+
+  }
+}
 
 listOfNames(people);
