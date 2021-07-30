51cf1365950cfad5ff10472a99a1bc9e07776265
Bimbola Lasisi <78049244+blasisi@users.noreply.github.com>
2021-07-30 21:43:22 +0100

    project incomplete

---
+++ projects/1-bike-project/js/main.js
@@ -0,0 +1,51 @@
+
+//Blue button sections 
+const blueButton = document.getElementById("blueBtn");
+ function clickToBlue(){
+     let jumbotronBlue = document.querySelector(".jumbotron");
+     const jumbotronBlue.style.background ="588fbd";
+      let donateBikeToday = document.getElementById("donate-btn");
+      donateBikeToday.style.background = "#ffa500";
+      let volunteer = document.getElementById =("volunteer-btn");
+      volunteer.style.background ="black";
+      volunteer.style.background ="white";
+ }
+ blueButton.addEventListener("click,clickToBlue");
+
+//Orange button section 
+const orangeButton = document.getElementById("orangeBtn");
+ function clickToOrange(){
+     let jumbotronOrange = document.querySelector(".jumbotron");
+     const  jumbotronOrange.style.background ="#f0ad4e";
+      let donateBikeToday = document.getElementById("donate-btn");
+      donateBikeToday.style.background = "#5751fd";
+      let volunteer = document.getElementById =("volunteer-btn");
+      volunteer.style.background ="#8c9c08";
+ }
+orangeButton.addEventListener("click,clickToOrange");
+
+ const greeButton = document.getElementById("greenBtn");
+ function clickToGreen(){
+     let jumbotronGreen = document.querySelector(".jumbotron");
+     let jumbotronGreen.style.background ="87ca8a";
+      let donateBikeToday = document.getElementById("donate-btn");
+      donateBikeToday.style.background = "black";
+      let volunteer = document.getElementById =("volunteer-btn");
+      volunteer.style.background ="bc9c08";
+ };
+
+ greeButton.addEventListener("click,clickToGreen");
+ const summitButton = document.getElementById("summit-Button");
+ summitButton.addEventListener("click, formValidation");
+ function formValidation(event){
+     event.preventDefault();
+     const emailInput = document.querySelector("exampleInputEmail");
+     const nameInput= document.querySelector("#example-text-input");
+     const describeYourSelf= document.querySelector("exampleTextArea");
+     const regExpression =  /[A-Z][A-Za-z']*\s(?:[A-Z]\.?\s)?[A-Z][A-Za-z']*/g,
+       
+ }
+
+
+
+
